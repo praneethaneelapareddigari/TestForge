cd8da76601f6a176c822000a14951ee08730b33a
Praneetha Neelapareddigari <praneethaneelapareddigari@gmail.com>
2025-09-10 05:29:30 +0530

    Fix imports for CI / TestForge review

--- examples/sample_pkg/tests_generated/test_module_a_add.py
+++ examples/sample_pkg/tests_generated/test_module_a_add.py
@@ -1,7 +1,9 @@
-import pytest
-from ..module_a import add
+import sys
+import os
 
-def test_add_smoke():
-    res = add()
-    assert res is not None
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from sample_pkg.module_a import add
+
+def test_add():
+    assert add(2, 3) == 5
